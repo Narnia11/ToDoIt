d0b22d66e47a4a4fffb52051909e2eed51e74f10
narges hekmatnia <narges.hekmatnia1983@gmail.com>
2022-02-28 22:16:42 +0100

    Private fields(Id, firstName, lastName) in Person Class & constructor with objects(fields) added.

--- ToDoIt/Models/Person.cs
+++ ToDoIt/Models/Person.cs
@@ -6,5 +6,12 @@ namespace ToDoIt.Models
 {
     class Person
     {
+        private readonly int Id;
+        private string firstName;
+        private string lastName;
+        public Person(int Id, string firstname, string lastname)
+        {
+
+        }
     }
 }
